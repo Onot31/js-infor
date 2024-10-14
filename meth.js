b10b2e4f0bc1d9242bf67626761a3b95ffed3c85
Onot31 <mfr145a@gmail.com>
2024-10-14 08:56:25 +0700

    komit kedua

---
+++ meth.js
@@ -0,0 +1,9 @@
+for (let i = 1; i < 8; i++) {
+  if (i < 4) {
+    document.write(`Speaker ruang kelas ${i} berfungsi dengan baik`);
+  } else if ((i = 4)) {
+    document.write(`Speaker ruang kelas ${i} sedang diperbaiki`);
+  } else {
+    document.write(`Speaker ruang kelas ${i} tidak berfungsi`);
+  }
+}
