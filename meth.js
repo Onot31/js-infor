90b1b6e323068a8f5fe4a6831cf247e07245a9ec
Onot31 <137066709+Onot31@users.noreply.github.com>
2024-10-14 09:03:50 +0700

    komit ketiga

--- meth.js
+++ meth.js
@@ -1,8 +1,8 @@
 for (let i = 1; i < 8; i++) {
   if (i < 4) {
-    document.write(`Speaker ruang kelas ${i} berfungsi dengan baik`);
+    console.log(`Speaker ruang kelas ${i} berfungsi dengan baik`);
   } else if ((i = 4)) {
-    document.write(`Speaker ruang kelas ${i} sedang diperbaiki`);
+    console.log(`Speaker ruang kelas ${i} sedang diperbaiki`);
   } else {
     console.log(`Speaker ruang kelas ${i} tidak berfungsi`);
   }
